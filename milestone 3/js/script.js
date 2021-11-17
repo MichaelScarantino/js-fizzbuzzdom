05551ebcd9e75e7877016e8c150dd60424e85a98
MichaelScarantino <scarantino.michael@outlook.it>
2021-11-17 09:20:42 +0100

    correct title mileston 3

--- milestone 3/js/script.js
+++ milestone 3/js/script.js
@@ -1,7 +1,6 @@
 // Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, 
 // per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
-// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 
 let boxContainer = document.querySelector('.container');
 
